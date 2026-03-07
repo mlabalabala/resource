24e0da07e8dc95ced4f1a64c3015f31149cbdb83
satan.xyz@qq.com <91363385+mlabalabala@users.noreply.github.com>
2026-03-07 17:21:55 +0800

    Update daynodefree.sh

--- scripts/daynodefree.sh
+++ scripts/daynodefree.sh
@@ -5,7 +5,7 @@ y=$(echo $datetime | awk -F',' '{print $1}')
 m=$(echo $datetime | awk -F',' '{print $2}')
 d=$(echo $datetime | awk -F',' '{print $3}')
 #echo $y-$m-$d
-daysuburl="https://nodefree.githubrowcontent.com/$y/$m/$y$m$d.txt"
+daysuburl="https://node.nodefree.me/$y/$m/$y$m$d.txt"
 outfile="nodefree.txt"
 #echo $daysuburl
 curl -sk $daysuburl -o $outfile
