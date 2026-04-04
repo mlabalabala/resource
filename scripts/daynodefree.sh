d3ccd2cdac38e167a966b439be222ae93d972d2e
satan.xyz@qq.com <91363385+mlabalabala@users.noreply.github.com>
2026-04-04 16:25:07 +0800

    Update daynodefree.sh

--- scripts/daynodefree.sh
+++ scripts/daynodefree.sh
@@ -11,15 +11,14 @@ nodefreeendata=$(curl -sk $daysuburl)
 nodefreedata=$(echo $nodefreeendata | base64 -d) 
 
 daysuburl="https://yoyapai.com/mianfeijiedian/$y$m$d-ssr-v2rayvpn-jiedian-yoyapai.com.txt"
-echo $daysuburl
 statuscode=$(curl -sI -o /dev/null -w "%{http_code}\n" $daysuburl)
-echo $statuscode
+#echo $statuscode
 [ "$statuscode" = "404" ] && {
     daysuburl=$(echo $daysuburl | sed 's/v2rayvpn/v2ray-vpn/g')
-    echo $daysuburl
 }
+echo $daysuburl
 
-yoyapaidata=$(curl -sk $daysuburl)
+yoyapaidata=$(curl -sk $daysuburl | tail -n +6)
 
 outfile="nodefree.txt"
 { echo "$nodefreedata"; echo "$yoyapaidata"; } | base64 | tr -d '\n' > $outfile
