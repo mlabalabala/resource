b1bd0c0cd62eb970f7645c1614c4f3f48755020a
satan.xyz@qq.com <91363385+mlabalabala@users.noreply.github.com>
2026-05-14 10:14:45 +0800

    Update alpine-armhf-3.20-install.sh

--- scripts/alpine-armhf-3.20-install.sh
+++ scripts/alpine-armhf-3.20-install.sh
@@ -55,7 +55,7 @@ command+=" PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/us
 command+=" TERM=\$TERM"
 command+=" LANG=C.UTF-8"
 command+=" TZ=Asia/Shanghai"
-command+=" /bin/bash --login"
+command+=" /bin/ash --login"
 com="\$@"
 if [ -z "\$1" ];then
     exec \$command
